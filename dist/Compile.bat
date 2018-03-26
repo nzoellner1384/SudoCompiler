b7a50411e2ec7b1c7035d2de7a7ee37eecfaac58
Nicholas Zoellner <nzoellner1384@gmail.com>
2018-03-26 18:24:51 -0500

    _

--- dist/Compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal ENABLEDELAYEDEXPANSION
-set file=%1
-set type=.java
-set blank=""
-java -jar SudoCompiler.jar %file%
-call set file=%file:.n=!type!%
-javac %file%
-@echo Finished!
